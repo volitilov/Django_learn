5596f64cc3156ed1eb4aa116b2be51d978581e87
volitilov <volitilov@gmail.com>
2017-04-30 11:59:06 +0300

    My Django_learn app, first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/volitilov/git/Django_learn/bin/python3
+# EASY-INSTALL-SCRIPT: 'Django==1.11','django-admin.py'
+__requires__ = 'Django==1.11'
+__import__('pkg_resources').run_script('Django==1.11', 'django-admin.py')
