d433fcbbfd54eaa875497273aea41e09dd53e6f8
volitilov <volitilov@gmail.com>
2017-10-12 18:03:00 +0300

    add: django info

--- bin/django-admin.py
+++
@@ -1,4 +0,0 @@
-#!/home/volitilov/git/Django_learn/bin/python3
-# EASY-INSTALL-SCRIPT: 'Django==1.11','django-admin.py'
-__requires__ = 'Django==1.11'
-__import__('pkg_resources').run_script('Django==1.11', 'django-admin.py')
---
+++ info/request_response.py
@@ -0,0 +1,2 @@
+# Объекты ответа и запроса
+# https://djbook.ru/rel1.9/ref/request-response.html#module-django.http
