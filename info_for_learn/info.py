692ee239da92dca5b9ed1653617a1cc1fc2e5475
volitilov <volitilov@gmail.com>
2018-03-21 19:49:02 +0300

    add new info

---
+++ info_for_learn/info.py
@@ -0,0 +1,107 @@
+Django ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+# Django - это высокоуровневая система Python Web, которая поощряет быстрое 
+# развитие и чистый, прагматичный дизайн. Построенный опытными 
+# разработчиками, он заботится о многих проблемах веб-разработки, поэтому 
+# вы можете сосредоточиться на написании своего приложения без необходимости 
+# изобретать колесо. 
+
+# документация на английском:
+# https://www.djangoproject.com/
+
+# на русском:
+# https://djbook.ru/
+# https://djangofan.ru/
+
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+API Reference
+
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+Applications
+# Django содержит реестр установленных приложений, который хранит 
+# конфигурацию и обеспечивает интроспекцию. Он также ведет список доступных 
+# моделей.
+# смотреть папку applications
+
+System check framework
+#
+
+Built-in class-based views API
+#
+
+Clickjacking Protection
+#
+
+contrib packages
+#
+
+Cross Site Request Forgery protection
+#
+
+Databases
+#
+
+django-admin and manage.py
+#
+
+Running management commands from your code
+#
+
+Django Exceptions
+#
+
+File handling
+#
+
+Forms
+#
+
+Middleware
+#
+
+Migration Operations
+#
+
+Models
+#
+
+Request and response objects
+#
+
+SchemaEditor
+#
+
+Settings
+#
+
+Signals
+#
+
+Templates
+#
+
+TemplateResponse and SimpleTemplateResponse
+#
+
+Unicode data
+#
+
+django.urls utility functions
+#
+
+django.urls functions for use in URLconfs
+#
+
+django.conf.urls functions for use in URLconfs
+#
+
+Django Utils
+#
+
+Validators
+#
+
+Built-in Views
+#
